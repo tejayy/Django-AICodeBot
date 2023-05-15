35a973f7cc875514ae58e33c59bd4efa4f0eb565
star <tbj456@gmail.com>
2023-05-15 22:11:58 +0530

    Almost Final Commit

--- codebot/website/views.py
+++ codebot/website/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 import openai
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from .models import Code
 
 
 # Create your views here.
 def home(request):
-    # API = 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
+    # API = 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
     lang_list = [
         "c",
         "clike",
@@ -53,7 +57,7 @@ def home(request):
             )
         else:
             """OpenAI Key"""
-            openai.api_key = "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"
+            openai.api_key = "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"
             """Create OpenAi Instance"""
             openai.Model.list()
             """Make An OpenAi Request"""
@@ -134,7 +138,7 @@ def suggest(request):
             )
         else:
             """OpenAI Key"""
-            openai.api_key = "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"
+            openai.api_key = "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"
             """Create OpenAi Instance"""
             openai.Model.list()
             """Make An OpenAi Request"""
@@ -165,3 +169,59 @@ def suggest(request):
                 )
 
     return render(request, "suggest.html", {"lang_list": lang_list})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in..')
+            return redirect('home')
+        else:
+            messages.success(request, 'Error Loggin In..')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'you have been logged out! Have a nice Day..')
+    return redirect('home')
+    
+def register_user(request):
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Registered...Congrats!!")
+			return redirect('home')
+
+	else:
+		form = SignUpForm()
+
+	return render(request, 'register.html', {"form": form})
+
+
+def past(request):
+	if request.user.is_authenticated:
+		code = Code.objects.filter(user_id=request.user.id)
+		return render(request, 'past.html', {"code":code})	
+	else:
+		messages.success(request, "You Must Be Logged In To View This Page")
+		return redirect('home')
+
+
+def delete_past(request, Past_id):
+	past = Code.objects.get(pk=Past_id)
+	past.delete()
+	messages.success(request, "Deleted Successfully...")
+	return redirect('past')
+    
+    
